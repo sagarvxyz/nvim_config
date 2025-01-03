3e9c477f0ced3e60dd35cc33516c84092bebceb8
Sagar Velagala <sagar.velagala@gmail.com>
2025-01-02 22:26:25 -0500

    add lsp config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,56 +1,65 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-tresitter.install").update({ with_sync = true })()
-	end,
-
-	config = function()
-		require'nvim-treesitter.configs'.setup ({
-		  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-		ensure_installed = { 
-			"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", 
-		  	"javascript", "typescript", "jsdoc", "python", "sql", "yaml" 
-		},
-
-		  -- Install parsers synchronously (only applied to `ensure_installed`)
-		  sync_install = false,
-
-		  -- Automatically install missing parsers when entering buffer
-		  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-		  auto_install = true,
-
-		  -- List of parsers to ignore installing (or "all")
-		  ignore_install = { },
-
-		  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-		  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-		  indent = {
-			  enable = true
-		  },
-
-		  highlight = {
-		    enable = true,
-
-		    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		    -- the name of the parser)
-		    -- list of language that will be disabled
-		    disable = { },
-		    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		    disable = function(lang, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-			    return true
-			end
-		    end,
-
-		    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-		    -- Instead of true it can also be a list of languages
-		    additional_vim_regex_highlighting = { "markdown" },
-		  }
-		})
-	end
+  "nvim-treesitter/nvim-treesitter",
+  version = false, -- last release is way too old and doesn't work on Windows
+  build = ":TSUpdate",
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  keys = {
+    { "<c-space>", desc = "Increment Selection" },
+    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+  opts_extend = { "ensure_installed" },
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "c",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "sql",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+    textobjects = {
+      move = {
+        enable = true,
+        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+        goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+        goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+      },
+    },
+  },
+  ---@param opts TSConfig
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      opts.ensure_installed = LazyVim.dedup(opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,14 +2,9 @@ return {
 	'nvim-telescope/telescope.nvim',
 	tag='0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
-
-	config = function()
-		require('telescope').setup({})
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-		vim.keymap.set('n', '<leader>fs', function()
-			builtin.grep_string( { search = vim.fn.input("Grep > ") }) 
-		end)
-	end
+	keys = {
+		{ mode = { "n" }, "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files respecting .gitignore" },
+		{ mode = { "n" }, "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find files tracked in git, respecting .gitignore" },
+		{ mode = { "n" }, "<leader>fs", function() require("telescope.builtin").grep_string( { search = vim.fn.input("Grep > ") }) end}
+	}
 }
